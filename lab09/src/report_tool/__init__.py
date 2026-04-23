3949f28a95e9ef972559fbde6d45960f84a791c2
Regomal <rmalakhov7@gmail.com>
2026-04-24 00:27:57 +0200

    lab09

--- lab09/src/report_tool/__init__.py
+++ lab09/src/report_tool/__init__.py
@@ -1,13 +1,13 @@
 """
-Report Tool - A clean and structured numeric report generator.
+Report Tool - A structured numeric report generator
 """
 
-# Подтягиваем только публичные функции из наших модулей
+# We only import public functions
 from .data import parse_numbers, analyze_numbers
 from .formatter import build_report, build_sorted_report
 from .storage import save_report, read_back
 
-# Явно указываем, что доступно при импорте со звездочкой (from report_tool import *)
+# We explicitly specify what is available during import (from report_tool import *)
 __all__ = [
     "parse_numbers",
     "analyze_numbers",
